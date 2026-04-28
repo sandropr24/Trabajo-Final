598669c2a93789d616640d8968674019fa92b9e2
Sandro Pachas <sandroromanipr24@gmail.com>
2026-04-28 18:21:55 -0500

    server.js update

--- server.js
+++ server.js
@@ -6,26 +6,21 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Página inicial: LOGIN
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
-// Archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rutas API
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/herramientas', require('./routes/herramientas'));
 app.use('/api/prestamos', require('./routes/prestamos'));
 
-// Ruta de prueba API
 app.get('/api', (req, res) => {
   res.json({
     success: true,
@@ -33,7 +28,6 @@ app.get('/api', (req, res) => {
   });
 });
 
-// Error handler global
 app.use((err, req, res, next) => {
   console.error('Error no manejado:', err.stack);
   res.status(500).json({
@@ -42,7 +36,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
   console.log(`Login:             http://localhost:${PORT}`);
